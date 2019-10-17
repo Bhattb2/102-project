e265b9ee0f073f9c13dee7e32635cba58040b4c0
Bhagirath Bhatt <43795438+Bhattb2@users.noreply.github.com>
2019-10-17 10:30:17 -0700

    Update jsq.js

--- jsq.js
+++ jsq.js
@@ -1,11 +1,16 @@
 # Javascript & jQuery
 
-- Javascript
+- JavaScript
     - JavaScript can be used in browsers to make websites more interactive, interesting, and user-friendly. It involves:
         - Basic porgaramming concepts
         - Language
-        - How it os applied
+        - How it is applied
         
--         
+ - JavaScript allows you to make web pages nore interactive by accessing and modigying the content and markup used in a web page while it is being viewd in browser.
+        - Access the content of the page
+        - Modify the content of the page
+        - Program rules or instrictions the browser can follow
+        - React ot events triggered by the user or browser
+        
     
         
